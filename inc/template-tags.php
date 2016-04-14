07c45e93922c6de2aa1eee4b9ef4624c0b405280
Gabriel Trisca <gtrisca@cignifi.com>
2016-04-14 00:09:00 -0400

    remove updated class from post meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'tog000io_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function tog000io_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"><span class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>%4$s</time>';
 	}
